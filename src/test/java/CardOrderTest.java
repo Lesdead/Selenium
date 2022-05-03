b378aff830c8e375d6b17019c6c407134289401a
lesdead <lesdead@gmail.com>
2022-05-03 12:25:43 +0300

    3 try

--- src/test/java/CardOrderTest.java
+++ src/test/java/CardOrderTest.java
@@ -15,24 +15,25 @@ class CardOrderTest {
 
     WebDriver driver;
 
-    ChromeOptions options = new ChromeOptions();{
-    options.addArguments("--disable-dev-shm-usage");
-    options.addArguments("--no-sandbox");
-    options.addArguments("--headless");
-    driver = new ChromeDriver(options);}
-
     @BeforeAll
     static void setupClass() {WebDriverManager.chromedriver().setup();}
     @BeforeEach
     void setupTest() {driver = new ChromeDriver();}
     @AfterEach
     void close() {if (driver != null) {driver.quit();}
+
+    ChromeOptions options = new ChromeOptions();
+    options.addArguments("--disable-dev-shm-usage");
+    options.addArguments("--no-sandbox");
+    options.addArguments("--headless");
+    driver = new ChromeDriver(options);
     }
+
 
     @Test
     public void validTest() {
         driver.get("http://localhost:9999");
-        
+
         driver.findElement(By.cssSelector("[type=\"text\"]")).sendKeys("Пупкин Вася");
         driver.findElement(By.cssSelector("[type=\"tel\"]")).sendKeys("+12345678901");
         driver.findElement(By.className("checkbox__box")).click();
